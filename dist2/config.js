2cde11ac8d0be97d626699738467e28bae53909e
joker <julio_santibanez@hotmail.com>
2024-12-13 05:06:15 -0300

    terminado

--- dist2/config.js
+++
@@ -1,6 +0,0 @@
-export const configuration = {
-    username: /*process.env.USERNAME || */ 'Julio Santibanez', // se obtiene de variable de entorno o por defecto 'default'
-    apikey: process.env.APIKEY || 'api-key', // se obtiene de variable de entorno o por defecto 'default-key'
-    port: process.env.PORT || 3000, // se obtiene de variable de entorno o por defecto 3000
-};
-//# sourceMappingURL=config.js.map
